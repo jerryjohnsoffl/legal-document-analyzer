cf7b1a7766e73a0938fa8fa53af7dba747b007f6
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-07-28 00:37:04 +0530

    result added

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Result = () => {
   return (
     <div className="w-3/5 my-8 px-4">
-        <h3 className="text-3xl text-black">Result</h3>
+        <h3 className="text-2xl md:text-3xl lg:text-4xl font-medium text-black">Results</h3>
+        <div className="mx-2">Summary</div>
+        <div className="flex">
+            <div className="w-1/2 mx-2">Risks Found</div>
+            <div className="w-1/2 mx-2">Suggestions</div>
+        </div>
 
     </div>
   )
