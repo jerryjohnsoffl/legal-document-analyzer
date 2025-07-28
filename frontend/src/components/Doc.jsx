cb29472da5490352c8445bb77cc729e795cd5001
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-07-28 16:27:49 +0530

    Updated

--- frontend/src/components/Doc.jsx
+++ frontend/src/components/Doc.jsx
@@ -3,7 +3,7 @@ import DocImg from '../assets/doc.png'
 
 const Doc = () => {
   return (
-    <div className="border-2 bg-dull-white border-grey rounded-xl border-dashed w-3/5 h-52 p-4 flex flex-col justify-center items-center">
+    <div className="border-2 bg-dull-white border-grey rounded-xl border-dashed w-4/5 md:w-3/5 h-52 p-4 flex flex-col justify-center items-center">
         <img src={DocImg} alt="" className='h-8 md:h-14 my-4'/>
         <div className="flex flex-col">
           <label className='py-2 px-4' for="myfile">Upload a legal document:</label>
