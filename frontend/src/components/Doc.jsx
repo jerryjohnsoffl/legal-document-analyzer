e811160e7d16dd020f5b9022bbc4e8a54daf11b2
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-07-28 12:54:38 +0530

    app.py added

--- frontend/src/components/Doc.jsx
+++ frontend/src/components/Doc.jsx
@@ -5,7 +5,10 @@ const Doc = () => {
   return (
     <div className="border-2 border-grey rounded-xl border-dashed w-3/5 h-52 p-4 flex flex-col justify-center items-center">
         <img src={DocImg} alt="" className='h-8 md:h-14 my-4'/>
-        <div className="">Upload a legal document</div>
+        <div className="flex flex-col">
+          <label className='py-2 px-4' for="myfile">Upload a legal document:</label>
+          <input className="bg-def-black py-2 px-4 rounded-lg flex justify-center text-white" type="file" name="" id="myfile" />
+          </div>
     </div>
   )
 }
