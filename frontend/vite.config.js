88162ff28f33afa5376bc25d45415ff79d3a5bfd
jerryjohnsoffl <jerryjohnsoffl@gmail.com>
2025-08-04 03:14:04 +0530

    Backend updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   base: "/legal-document-analyzer",
+  server: {
+    proxy: {
+      '/upload': {
+        target: 'http://localhost:5173',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/upload/, ''),
+      }
+    }
+  },
 })
